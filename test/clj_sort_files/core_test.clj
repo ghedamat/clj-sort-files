ae74f667d6cba4d542c4116faaa3335c4dd1120a
Mattia Gheda <ghedamat@gmail.com>
2014-04-13 11:48:47 -0400

    first test

--- test/clj_sort_files/core_test.clj
+++ test/clj_sort_files/core_test.clj
@@ -2,6 +2,11 @@
   (:use clojure.test
         clj-sort-files.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn- get-current-directory []
+  (. (java.io.File. ".") getCanonicalPath))
+
+(deftest read-dir-test
+  (testing "read-dir reads all files in a dir"
+    (is (= 2 (count (read-dir "test/fixtures"))))))
+
+(run-tests)
