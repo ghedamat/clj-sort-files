1785e665a769d9452fb4812e0b2d0cc44791a7f0
Mattia Gheda <ghedamat@gmail.com>
2014-04-22 21:19:48 -0400

    great refactoring thanks to @jamesaoverton !!!
    amazing night at the Toronto Clojure meetup

--- src/clj_sort_files/core.clj
+++ src/clj_sort_files/core.clj
@@ -4,17 +4,6 @@
   (:use [clj-sort-files.cli])
   (:gen-class))
 
-(defn- reverse-cmp [a b]
-  (compare b a))
-
-;; taken from https://groups.google.com/forum/#!topic/clojure-dev/9ctJC-LXNps
-(defn- apply-kw
-  "Like apply, but f takes keyword arguments and the last argument is
-  not a seq but a map with the arguments for f"
-  [f & args]
-  {:pre [(map? (last args))]}
-  (apply f (apply concat
-                  (butlast args) (last args))))
 
 (defn read-dir
   "Return a seq of the files contained in a directory"
@@ -34,33 +23,40 @@
 
 (defn sort-files
   "Returns a sorted sequence of file-hash-maps,
-  where the sort field is one of the keys.
-  Sort order is descending by default"
-  ([files field]
-   (sort-files files field reverse-cmp))
-  ([files field comparatorfn]
-   (sort-by field comparatorfn
-         (map (fn [file] (file-hash-map file))
-              files))))
+  where the sort field is one of the keys."
+  [files field direction]
+  (->> files
+       (map file-hash-map)
+       (sort-by field)
+       direction))
 
-(defn sort-paths
+(comment
+  (defn sort-paths
+    "Returns an array of hashes (one per path)
+    where the key is the path and the value is a sorted array
+    of the contained files"
+    [paths {:keys [sort-field sort-order sort-limit]
+            :or {sort-field :updated sort-order :desc}}]
+    (->> paths
+         (map (juxt identity read-dir))
+         (map (fn [[path files]]
+                {path (take (or sort-limit (count files))
+                      (sort-files files
+                                  sort-field
+                                  (if (= sort-order :asc) identity reverse)))}))))
+  )
+
+(defn sort-path
   "Returns an array of hashes (one per path)
   where the key is the path and the value is a sorted array
   of the contained files"
-  [paths & options]
-  (let [{:keys [sort-field sort-order sort-limit]
-         :or {sort-field :updated sort-order :desc}} options
-        cmp (if (= sort-order :asc)
-              compare
-              reverse-cmp)]
-    (map (fn [path]
-           (let [files
-                 (if sort-limit
-                   (take sort-limit (sort-files (read-dir path) sort-field cmp))
-                   (sort-files (read-dir path) sort-field cmp))]
-             {path files}))
-         paths)))
-
+  [path {:keys [sort-field sort-order sort-limit]
+         :or {sort-field :updated sort-order :desc}}]
+  (let [files (read-dir path)]
+    {path (take (or sort-limit (count files))
+                (sort-files files
+                            sort-field
+                            (if (= sort-order :asc) identity reverse)))}))
 
 
 (defn -main [& args]
@@ -73,15 +69,11 @@
     ;; Execute program with options
     (if (seq arguments)
       (try
-        (json/pprint (apply-kw sort-paths arguments options)
+        (json/pprint (map #(sort-path % options) arguments)
                    :escape-slash false)
         (catch clojure.lang.ExceptionInfo e
           (if (= :path-not-found (-> e ex-data :cause))
             (exit 1 (str "Directory not found: " (-> e ex-data :path)))
-            (throw e)))
-        )
+            (throw e))))
 
       (exit 1 (usage summary)))))
-
-
-
